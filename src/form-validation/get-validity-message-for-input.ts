e5fb94b19a3f588455d050d75d2cfc1d59daa3c1
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2023-08-28 06:35:44 -0700

    Add all validity message logic

--- src/form-validation/get-validity-message-for-input.ts
+++ src/form-validation/get-validity-message-for-input.ts
@@ -1,67 +1,167 @@
 /**
- * Uses the Constraint Validation ValidityState API to return a custom validity message
+ * Use the Constraint Validation ValidityState API to return a custom validity message
  * @param {HTMLInputElement} inputEl The input element
  * @returns {string} A custom validity message for the given input element
  */
+// eslint-disable-next-line complexity
 export const getValidityMessageForInput = (
   inputEl: HTMLInputElement
 ): string => {
   // If the input is valid, return an empty string.
   if (inputEl.validity.valid) return '';
 
-  // If the input is invalid, return the appropriate error message.
-  if (inputEl.validity.valueMissing) {
-    return `Please provide a value for the "${inputEl.labels
-      ?.item(0)
-      .textContent?.trim()}" field.`;
+  /**
+   * Validation constraints:
+   * - required
+   */
+  if (inputEl.name === 'customerFirstName' && inputEl.validity.valueMissing) {
+    return 'Please enter your first name. (This field is required.)';
   }
-  if (inputEl.validity.typeMismatch) {
-    if (inputEl.type === 'email') {
-      return 'Please provide a valid email address.';
+
+  /**
+   * Validation constraints:
+   * - required
+   */
+  if (inputEl.name === 'customerLastName' && inputEl.validity.valueMissing) {
+    return 'Please enter your last name. (This field is required.)';
+  }
+
+  /**
+   * Validation constraints:
+   * - required
+   * - type=email
+   */
+  if (inputEl.name === 'customerEmail') {
+    if (inputEl.validity.valueMissing) {
+      return 'Please enter an email address. (This field is required.)';
     }
-    if (inputEl.type === 'url') {
-      return 'Please provide a valid URL.';
+    if (inputEl.validity.typeMismatch) {
+      return 'Please enter a valid email address.';
     }
   }
-  if (inputEl.validity.tooShort) {
-    const currCharCount = inputEl.value.length;
-    return `Please make the text longer, it should be at least ${inputEl.getAttribute(
-      'minLength'
-    )} characters. Right now, it's only ${currCharCount} ${
-      currCharCount === 1 ? 'character' : 'characters'
-    }.`;
+
+  /**
+   * Validation constraints:
+   * - required
+   * - type=date
+   * - min
+   * - max
+   */
+  if (inputEl.name === 'purchaseDate') {
+    if (inputEl.validity.valueMissing) {
+      return 'Please enter a date. (This field is required.)';
+    }
+    if (inputEl.validity.typeMismatch) {
+      return 'Please enter a valid date.';
+    }
+    if (inputEl.validity.rangeUnderflow) {
+      return `The purchase date must be within the last calendar year.`;
+    }
+    if (inputEl.validity.rangeOverflow) {
+      return `The purchase date cannot be a future date.`;
+    }
   }
-  if (inputEl.validity.tooLong) {
-    const currCharCount = inputEl.value.length;
+
+  /**
+   * Validation constraints:
+   * - required
+   * - type=url
+   */
+  if (inputEl.name === 'demoUrl') {
+    if (inputEl.validity.valueMissing) {
+      return 'Please enter a URL. (This field is required.)';
+    }
+    if (inputEl.validity.typeMismatch) {
+      return 'Please enter a valid URL.';
+    }
+  }
+
+  /**
+   * Validation constraints:
+   * - required
+   * - minLength
+   */
+  if (inputEl.name === 'demoTooShort') {
+    if (inputEl.validity.valueMissing) {
+      return 'Please enter a text value. (This field is required.)';
+    }
+    if (inputEl.validity.tooShort) {
+      const minLength = inputEl.getAttribute('minLength');
+      const difference = Number(minLength) - inputEl.value.length;
+      return `Please enter at least ${minLength} characters. (Add ${difference} more ${
+        difference === 1 ? 'character' : 'characters'
+      }.)`;
+    }
+  }
+
+  /**
+   * Validation constraints:
+   * - maxLength
+   */
+  if (inputEl.name === 'demoTooLong' && inputEl.validity.tooLong) {
     return `Please shorten this text to a maximum of ${inputEl.getAttribute(
       'maxLength'
     )} characters. It currently exceeds that limit.`;
   }
-  if (inputEl.validity.rangeUnderflow) {
-    return `Please choose a number that is ${inputEl.getAttribute(
-      'min'
-    )} or more.`;
+
+  /**
+   * Validation constraints:
+   * - required
+   * - type=number
+   * - min
+   * - max
+   * - step
+   */
+  if (inputEl.name === 'demoRangeEven') {
+    if (inputEl.validity.valueMissing) {
+      return 'Please enter a number. (This field is required.)';
+    }
+    if (inputEl.validity.badInput) {
+      return 'Please enter a valid number value.';
+    }
+    if (
+      inputEl.validity.rangeUnderflow ||
+      inputEl.validity.rangeOverflow ||
+      inputEl.validity.stepMismatch
+    ) {
+      return `The value should be an even number between ${inputEl.getAttribute(
+        'min'
+      )} and ${inputEl.getAttribute('max')}.`;
+    }
   }
-  if (inputEl.validity.rangeOverflow) {
-    return `Please choose a number that is ${inputEl.getAttribute(
-      'max'
-    )} or less.`;
+
+  /**
+   * Validation constraints:
+   * - type=number
+   * - min
+   * - max
+   * - step
+   */
+  if (inputEl.name === 'demoRangeOdd') {
+    if (inputEl.validity.valueMissing) {
+      return 'Please enter a number. (This field is required.)';
+    }
+    if (inputEl.validity.badInput) {
+      return 'Please enter a valid number value.';
+    }
+    if (
+      inputEl.validity.rangeUnderflow ||
+      inputEl.validity.rangeOverflow ||
+      inputEl.validity.stepMismatch
+    ) {
+      return `The value should be an odd number between ${inputEl.getAttribute(
+        'min'
+      )} and ${inputEl.getAttribute('max')}.`;
+    }
   }
-  if (inputEl.validity.stepMismatch) {
-    return `Please enter a valid value for the "${inputEl.labels
-      ?.item(0)
-      .textContent?.trim()}" field.`;
-  }
-  if (inputEl.validity.badInput) {
-    return `Please provide a valid value of type "${inputEl.getAttribute(
-      'type'
-    )}".`;
-  }
-  if (inputEl.validity.patternMismatch) {
-    return 'Please match the requested format.';
-  }
-  if (inputEl.validity.customError) {
-    return inputEl.validationMessage;
+
+  if (inputEl.name === 'demoPattern') {
+    if (inputEl.validity.tooShort || inputEl.validity.tooLong) {
+      return 'The code should be between 3 and 5 digits long.';
+    }
+    if (inputEl.validity.patternMismatch) {
+      return 'Please enter a valid 3-5 digit (number) code.';
+    }
   }
 
   // If all else fails, return a generic catchall error message.
