696bd7974d0634e6681a6ba6506d76464c41a095
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2023-08-31 04:50:09 -0700

    Delete unused function

--- src/form-validation/init.ts
+++ src/form-validation/init.ts
@@ -4,34 +4,6 @@ import { initJsValidate } from '../form-validation/init-js-validate.js';
 
 import { updateValidationStateForInput } from './update-validation-state-for-input.js';
 
-const initVestValidation = (formEl: HTMLFormElement) => {
-  // const jsValidateInputEls: NodeListOf<HTMLInputElement> =
-  // document.querySelectorAll('.js-validate');
-  // for (const inputEl of jsValidateInputEls) {
-  document
-    .querySelectorAll('.js-validate')
-    // eslint-disable-next-line @cloudfour/unicorn/no-array-for-each
-    .forEach((inputEl: HTMLInputElement) => {
-      // Set up `blur` and `input` validation for the inputs that can be validated
-      // with the Constraint Validation API.
-      inputEl.addEventListener('input', (event) => {
-        updateValidationStateForInput(event.target as HTMLInputElement);
-      });
-      inputEl.addEventListener('blur', (event) => {
-        updateValidationStateForInput(event.target as HTMLInputElement);
-      });
-      // Adding `aria-invalid` provides users who use assistive technology
-      // with a way to know if the input is invalid.
-      // Should initially be set to `false` before interaction/validity check.
-      inputEl.setAttribute('aria-invalid', 'false');
-
-      // Update the state for prefilled inputs.
-      if (inputEl.value !== '') {
-        updateValidationStateForInput(inputEl);
-      }
-    });
-};
-
 /**
  * Initialize validation setup
  */
