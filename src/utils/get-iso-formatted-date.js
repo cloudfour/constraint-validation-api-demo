2890d1a21eb55ffabea1a24893e9cdea8b098fd6
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2023-09-11 11:03:48 -0700

    Fix up for blog post

--- src/utils/get-iso-formatted-date.js
+++ src/utils/get-iso-formatted-date.js
@@ -1,16 +1,18 @@
 /**
- * Generates an ISO8601 formatted date string (YYYY-MM-DD)
+ * Generates an ISO 8601 formatted date string (YYYY-MM-DD) from a Date object.
  *
- * High-level, the goal is to return the value of `Date.prototype.toISOString()`
- * (specifically the first part, YYYY-MM-DD) so it can be used for a date input's
- * `min`/`max` values and also for setting date values (e.g. for automated tests).
+ * The goal is to return the value of Date.prototype.toISOString() without the
+ * time portion (e.g., '2023-03-21T04:15:47.000Z' becomes '2023-03-21').
+ * This is useful for setting a date input's min/max values and also for
+ * setting date values (e.g., for automated tests).
  *
- * The timezone returned by `Date.prototype.toISOString()` has a zero UTC offset.
+ * The timezone returned by Date.prototype.toISOString() has a zero UTC offset.
  *
  * Used without calculating a timezone offset, the date returned was off by a day.
- * To fix that, the logic below uses `Date.prototype.getTimezoneOffset()` to ensure
- * the value returned by `Date.prototype.toISOString()` matches local time zone.
+ * To fix that, the logic below uses Date.prototype.getTimezoneOffset() to ensure
+ * the value returned by Date.prototype.toISOString() matches local time zone.
  *
+ * @see https://en.wikipedia.org/wiki/ISO_8601
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
  * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Date_and_time_formats#date_strings
  * @see https://yagisanatode.com/2022/01/12/create-a-iso-string-from-date-string-input-intended-for-utc-date-in-javascript/
@@ -18,15 +20,20 @@
  * @returns {string}
  */
 export const getISOFormattedDate = (date) => {
-  // const date = new Date(date);
-  // Gets the difference in minutes between the time on the local computer and
-  // Universal Coordinated Time(UTC).
-  const timezoneOffset = date.getTimezoneOffset();
-  // Parse the date into milliseconds, then subtract the offset (in milliseconds)
-  const dateWithOffset = Date.parse(date) - timezoneOffset * 60 * 1000;
-  // Convert to full ISO format (e.g., '2023-03-21T04:15:47.000Z')
+  // First we need to get the timezone offset in milliseconds. The timezone
+  // offset is the difference between the time on the local computer and
+  // Universal Coordinated Time (UTC).
+  const timezoneOffsetInMinutes = date.getTimezoneOffset();
+  const timezoneOffsetInMilliseconds = timezoneOffsetInMinutes * 60 * 1000;
+
+  // Now, let's convert the date to milliseconds.
+  const dateInMilliseconds = Date.parse(date);
+
+  // Calculate the date with the timezone offset and convert to ISO format
+  // (e.g., '2023-03-21T04:15:47.000Z').
+  const dateWithOffset = dateInMilliseconds - timezoneOffsetInMilliseconds;
   const dateAsISOString = new Date(dateWithOffset).toISOString();
-  // Split to return ISO8601 format (YYYY-MM-DD)
+
+  // Split to return ISO8601 format (YYYY-MM-DD).
   return dateAsISOString.split('T')[0];
-  // return dateAsISOString.slice(0, Math.max(0, dateAsISOString.indexOf('T')));
 };
